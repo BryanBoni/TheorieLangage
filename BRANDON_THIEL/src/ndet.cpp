b601d94c72c9af92d199868e32c9523402d8c380
Bryan Boni <bryan.boni@etu.univ-lyon1.fr>
2016-11-28 09:30:10 +0100

    add Automate2Expr

--- BRANDON_THIEL/src/ndet.cpp
+++ BRANDON_THIEL/src/ndet.cpp
@@ -200,29 +200,6 @@ etatset_t Delta(const sAutoNDE& at, const etatset_t& e, symb_t c){
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-/*
-struct sAutoNDE{
-  // caractéristiques
-  size_t nb_etats;
-  size_t nb_symbs;
-  size_t nb_finaux;
-
-  etat_t initial;
-  // état initial
-
-  etatset_t finaux;
-  // états finaux : finaux_t peut être un int*, un tableau dynamique comme vector<int>
-  // ou une autre structure de donnée de votre choix.
-
-  trans_t trans;
-  // matrice de transition : trans_t peut être un int***, une structure dynamique 3D comme vector< vector< set<int> > >
-  // ou une autre structure de donnée de votre choix.
-  
-  epsilon_t epsilon; 
-  // transitions spontanées : epsilon_t peut être un int**, une structure dynamique 2D comme vector< set<int> >
-  // ou une autre structure de donnée de votre choix.
-};*/
-
 sAutoNDE Determinize(const sAutoNDE& at){
   sAutoNDE r;
 
@@ -401,15 +378,63 @@ sAutoNDE ExpressionRationnelle2Automate(string expr){
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+/*
 
-string Automate2ExpressionRationnelle(sAutoNDE at){
+typedef size_t                            etat_t;
+typedef unsigned char                     symb_t;
+typedef set< etat_t >                     etatset_t;
+typedef vector< vector< etatset_t > >     trans_t;
+typedef vector< etatset_t >               epsilon_t;
+typedef map< etatset_t, etat_t >          map_t;
+
+
+struct sAutoNDE{
+  // caractéristiques
+  size_t nb_etats;
+  size_t nb_symbs;
+  size_t nb_finaux;
+
+  etat_t initial;
+  // état initial
+
+  etatset_t finaux;
+  // états finaux : finaux_t peut être un int*, un tableau dynamique comme vector<int>
+  // ou une autre structure de donnée de votre choix.
+
+  trans_t trans;
+  // matrice de transition : trans_t peut être un int***, une structure dynamique 3D comme vector< vector< set<int> > >
+  // ou une autre structure de donnée de votre choix.
+  
+  epsilon_t epsilon; 
+  // transitions spontanées : epsilon_t peut être un int**, une structure dynamique 2D comme vector< set<int> >
+  // ou une autre structure de donnée de votre choix.
+};*/
+string Automate2ExpressionRationnelle(sAutoNDE at){//Bryan
   cout << "Construction d'une expression rationnelle à partir d'un automate\n";
 
   string sr;
 
-  //TODO définir cette fonction
+  //Etape 1 : arrangé l'automate. A Coder
+  sAutoNDE atPrime;//M'
 
-  return sr;
+  atPrime.nb_symbs = at.nb_symbs;
+  atPrime.nb_etats = at.nb_etats + 2;//M' = n+2 états
+  atPrime.nb_finaux = 1;
+
+  atPrime.initial = 0;
+
+  //Pseudo-code
+  for(vector< etatset_t> temp : at.trans){
+  	atPrime.trans[temp + 1] = at.trans;//décalge.
+  }
+
+  //epsilone pour etat 0 a état 1 et pour ancien finaux au nouvel état.
+
+  atPrime.finaux.insert(0, atPrime.nb_etats); //Attention il ne doit y avoir que un seul final.
+
+  //Etape 2 : le caractériser. A Coder
+
+  return str;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
